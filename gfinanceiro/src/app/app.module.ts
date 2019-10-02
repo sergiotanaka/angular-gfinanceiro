235d8b8dcef834e66819e5a6023c973bcb56dc2a
sergiotanaka <sergio_m_tanaka@hotmail.com>
2019-10-02 07:56:20 -0300

    Criando módulo e componente da conta

--- gfinanceiro/src/app/app.module.ts
+++ gfinanceiro/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountModule } from './modules/account/account.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ gfinanceiro/src/app/modules/account/account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gf-account',
+  templateUrl: './account.component.html',
+  styles: []
+})
+export class AccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
